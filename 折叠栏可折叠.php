cec499a97dd767294cb6932a08294147b2806127
km3xy <77920834+km3xy@users.noreply.github.com>
2024-01-25 21:47:59 +0800

    Create 折叠栏可折叠.php

---
+++ 折叠栏可折叠.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css">
+
+<script src="jquery-1.10.2/jquery.min.js">
+</script>
+
+
+<script src="jquery-1.10.2/jquery-1.10.2/jquery.min.js">
+</script>
+
+
+<script src="jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+
+</head>
+
+<body>
+
+​
+
+<div data-role="page" id="pageone">
+
+  <div data-role="header">
+
+    <h1 style="font-size:88px;color:#FF0000">可折叠块</h1>
+  </div>
+
+​
+
+  <div data-role="main" class="ui-content">
+
+    <div data-role="collapsible">
+
+     <h1>点击我 - 我可以折叠！</h1>
+
+      <p>我是可折叠的内容。</p>
+      
+      
+<br>
+
+
+
+
+
+<hr>
+
+<h1>
